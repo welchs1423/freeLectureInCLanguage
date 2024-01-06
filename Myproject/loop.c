412b582d66aa432635ceeb3a3d00b706a97cc23b
welchs1423 <welchs1423@naver.com>
2024-01-06 19:56:32 +0900

    뿔뿔

---
+++ Myproject/loop.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main(void) {
+	/*printf("Hello World\n");*/
+
+	// ++ ปิปิ
+	/*int a = 10;
+	printf("a ดย %d\n", a);
+	a++;
+	printf("a ดย %d\n", a);*/
+
+	//int b = 20;
+	//// b = b + 1;
+	//printf("b ดย %d\n", ++b);
+	//printf("b ดย %d\n", b++);
+	//printf("b ดย %d\n", b);
+
+	int i = 1;
+
+	printf("Hello World %d\n", i++);
+	printf("Hello World %d\n", i++);
+	printf("Hello World %d\n", i++);
+	printf("Hello World %d\n", i++);
+	printf("Hello World %d\n", i++);
+	printf("Hello World %d\n", i++);
+	printf("Hello World %d\n", i++);
+	printf("Hello World %d\n", i++);
+	printf("Hello World %d\n", i++);
+	printf("Hello World %d\n", i++);
+	printf("Hello World %d\n", i++);
+	printf("Hello World %d\n", i++);
+	printf("Hello World %d\n", i++);
+	printf("Hello World %d\n", i++);
+
+	return 0;
+}
--- Myproject/printfscanf.c
+++ Myproject/printfscanf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main(void)
+int main_printfscanf(void)
 {
 	// ¿Ã∏ß
 	char name[256];
