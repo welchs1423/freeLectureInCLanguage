69def9bdb126a840058fa1335246889ce61d027f
welchs1423 <welchs1423@naver.com>
2024-01-06 18:18:53 +0900

    scanf

--- Myproject/printfscanf.c
+++ Myproject/printfscanf.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
+
 int main(void)
 {
-	// 정수형 변수에 대한 예제
-	/*int age = 12;
-	printf("%d\n", age);
-	age = 13;
-	printf("%d\n", age);*/
+	/*int input;
+	printf("값을 입력하세요 : ");
+	scanf_s("%d", &input);
+	printf("입력값 : %d\n", input);*/
 
-	// 실수형 변수에 대한 예제
-	/*float f = 46.5;
-	printf("%.2f\n", f);
-	double d = 4.428;
-	printf("%.2lf\n", d);*/
-	//const int YEAR = 2000;	// 상수 (JAVA FINAL 같은 개념인듯?)
-	//prinff("태어난 년도 : %d\n", YEAR);
-	//YEAR = 2001;
+	/*int one, two, three;
+	printf("정수 3개를 입력하세요 : ");
+	scanf_s("%d %d %d", &one, &two, &three);
+	printf("첫 번째 값 : %d\n", one);
+	printf("두 번째 값 : %d\n", two);
+	printf("세 번째 값 : %d\n", three);*/
 
-	// printf
-	// 연산
-	//int add = 3 + 7; // 10
-	//printf("3 + 7 = %d\n", add);
-	//printf("%d x %d = %d\n", 30, 79, 30 * 79);
+	// 문자(한 글자), 문자열(한 글자 이상의 여러 글자)
+	/*char c = 'A';
+	printf("%c\n", c);*/
+
+	char str[256];
+	scanf_s("%s", str, sizeof(str));
+	printf("%s\n", str);
 
 	return 0;
 }
