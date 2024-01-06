7bea0af0a4af52af58e27f9d7f77340064578cd3
welchs1423 <welchs1423@naver.com>
2024-01-06 19:59:59 +0900

    for

--- Myproject/loop.c
+++ Myproject/loop.c
@@ -1,36 +1,14 @@
 #include <stdio.h>
 
 int main(void) {
-	/*printf("Hello World\n");*/
 
-	// ++ 뿔뿔
-	/*int a = 10;
-	printf("a 는 %d\n", a);
-	a++;
-	printf("a 는 %d\n", a);*/
+	// 반복문
+	// for, while, do while
 
-	//int b = 20;
-	//// b = b + 1;
-	//printf("b 는 %d\n", ++b);
-	//printf("b 는 %d\n", b++);
-	//printf("b 는 %d\n", b);
-
-	int i = 1;
-
-	printf("Hello World %d\n", i++);
-	printf("Hello World %d\n", i++);
-	printf("Hello World %d\n", i++);
-	printf("Hello World %d\n", i++);
-	printf("Hello World %d\n", i++);
-	printf("Hello World %d\n", i++);
-	printf("Hello World %d\n", i++);
-	printf("Hello World %d\n", i++);
-	printf("Hello World %d\n", i++);
-	printf("Hello World %d\n", i++);
-	printf("Hello World %d\n", i++);
-	printf("Hello World %d\n", i++);
-	printf("Hello World %d\n", i++);
-	printf("Hello World %d\n", i++);
+	//for (선언; 조건; 증감) 
+	for (int i = 1; i <= 10; i++) {
+		printf("Hello World %d\n", i);
+	}
 
 	return 0;
 }
