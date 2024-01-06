ac8da29842f051c271426b23eaafa67442c5f993
welchs1423 <welchs1423@naver.com>
2024-01-06 17:51:27 +0900

    상수

--- Myproject/printfscanf.c
+++ Myproject/printfscanf.c
@@ -7,8 +7,13 @@ int main(void)
 	age = 13;
 	printf("%d\n", age);*/
 
-	printf("1\n");
-	//printf("2\n");	// 2는 범위에서 벗어나므로 주석 처리
-	printf("3\n");
+	// 실수형 변수에 대한 예제
+	/*float f = 46.5;
+	printf("%.2f\n", f);
+	double d = 4.428;
+	printf("%.2lf\n", d);*/
+	const int YEAR = 2000;	// 상수 (JAVA FINAL 같은 개념인듯?)
+	prinff("태어난 년도 : %d\n", YEAR);
+	YEAR = 2001;
 	return 0;
 }
