fc905f4f8aca79bf2ae862f156d03779d441c9c9
welchs1423 <welchs1423@naver.com>
2024-01-06 20:04:15 +0900

    do while

--- Myproject/loop.c
+++ Myproject/loop.c
@@ -11,11 +11,17 @@ int main(void) {
 	}*/
 
 	// while (조건) { }
-	int i = 1;
+	/*int i = 1;
 	while (i <= 10) {
 		printf("Hello World %d\n", i);
 		i++;
-	}
+	}*/
+
+	// do {} while (조건);
+	int i = 1;
+	do {
+		printf("Hello World %d\n", i++);
+	} while (i <= 10);
 
 	return 0;
 }
