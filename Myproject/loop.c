b142c47688ed9db3c9575782d24655ec11c23931
welchs1423 <welchs1423@naver.com>
2024-01-06 23:09:04 +0900

    이중 반복문(구구단)

--- Myproject/loop.c
+++ Myproject/loop.c
@@ -18,10 +18,33 @@ int main(void) {
 	}*/
 
 	// do {} while (조건);
-	int i = 1;
-	do {
+	/*int i = 1;*/
+	/*do {
 		printf("Hello World %d\n", i++);
-	} while (i <= 10);
+	} while (i <= 10);*/
 
+	// 2중 반복문
+	/*for (int i = 1; i <= 3; i++) {
+		printf("첫 번째 반복문 : %d\n", i);
+
+		for (int j = 1; j <= 5; j++) {
+			printf("	두 번째 반복문 : %d\n", j);
+		}
+	}*/
+
+	// 구구단
+	// 2 x 1 = 2
+	// 2 x 2 = 4
+	// 2 x 3 = 6
+	// 2 x 4 = 8
+	// ...
+	// 9 x 9 = 81
+
+	/*for (int i = 2; i <= 9; i++) {
+		printf("%d단 계산\n", i);
+		for (int j = 1; j <= 9; j++) {
+			printf("	%d x %d = %d\n", i, j, i*j);
+		}
+	}*/
 	return 0;
 }
