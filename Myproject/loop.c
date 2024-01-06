791612bc241fbfb958f3eca073a513eb95798068
welchs1423 <welchs1423@naver.com>
2024-01-07 01:17:07 +0900

    프로젝트

--- Myproject/loop.c
+++ Myproject/loop.c
@@ -68,11 +68,25 @@ int main(void) {
 	*****
 	*/
 
-	for (int i = 0; i < 5; i++) {
-		for (int j = i; j < 5 - 1; j++) {
-			printf("S");
+	//for (int i = 0; i < 5; i++) {
+	//	for (int j = i; j < 5 - 1; j++) {
+	//		printf("S");
+	//	}
+	//	for (int k = 0; k <= i; k++) {
+	//		printf("*");
+	//	}
+	//	printf("\n");
+	//}
+
+	// 피라미드를 쌓아라 - 프로젝트
+	int floor;
+	printf("몇 층으로 쌓겠느냐?");
+	scanf_s("%d", &floor);
+	for (int i = 0; i < floor; i++) {
+		for (int j = i; j < floor - 1; j++) {
+			printf(" ");
 		}
-		for (int k = 0; k <= i; k++) {
+		for (int k = 0; k < i * 2 + 1; k = k++) {
 			printf("*");
 		}
 		printf("\n");
