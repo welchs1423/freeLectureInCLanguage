ea613474600bda56277447c11e49d3c1a35d2c3d
welchs1423 <welchs1423@naver.com>
2024-01-06 17:34:12 +0900

    주석

--- Myproject/printfscanf.c
+++ Myproject/printfscanf.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 int main(void)
 {
-	int age = 12;
+	// 정수형 변수에 대한 예제
+	/*int age = 12;
 	printf("%d\n", age);
 	age = 13;
-	printf("%d\n", age);
+	printf("%d\n", age);*/
+
+	printf("1\n");
+	//printf("2\n");	// 2는 범위에서 벗어나므로 주석 처리
+	printf("3\n");
 	return 0;
 }
