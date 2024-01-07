a50b49fae2c68d31402411ba323de0c64c836e2d
welchs1423 <welchs1423@naver.com>
2024-01-07 16:01:51 +0900

    break continue

--- Myproject/condition.c
+++ Myproject/condition.c
@@ -14,7 +14,7 @@ int main(void) {
 	// 초등학생(8~13) / 중학생(14~16) / 고등학생(17~19)으로 나누면?
 	// if / else if / else
 	int age = 25;
-	if (age >= 8 && age <= 13) {
+	/*if (age >= 8 && age <= 13) {
 		printf("초등학생입니다\n");
 	}
 	else if (age >= 14 && age <= 16) {
@@ -25,6 +25,30 @@ int main(void) {
 	}
 	else {
 		printf("학생이 아닌가봐요\n");
+	}*/
+
+	// break / continue
+	// 1번부터 30번까지 있는 반에서 1번에서 5번까지 조별 발표를 합니다.
+
+	/*for (int i = 1; i <= 30; i++) {
+		if (i >= 6) {
+			printf("나머지 학생은 집에 가세요\n");
+			break;
+		}
+		printf("%d번 학생은 조별 발표 준비를 하세요\n", i);
+	}*/
+
+	// 1번부터 30번까지 있는 반에서 7번 학생은 아파서 결석
+	// 7번을 제외하고 6번부터 10번까지 조별 발표를 하세요
+	for (int i = 1; i <= 30; i++) {
+		if (i >= 6 && i <= 10) {
+			if (i == 7) {
+				printf("%d 번 학생은 결석입니다\n", i);
+				continue;
+			}
+			printf("%d번 학생은 조별 발표 준비를 하세요\n", i);
+		}
 	}
+
 	return 0;
 }
