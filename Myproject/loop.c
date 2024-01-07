4920d586c1a1348c66b5cf629f630153bd41f31e
welchs1423 <welchs1423@naver.com>
2024-01-07 13:56:28 +0900

    if else

--- Myproject/loop.c
+++ Myproject/loop.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main(void) {
+int main_loop(void) {
 
 	// 반복문
 	// for, while, do while
@@ -79,18 +79,18 @@ int main(void) {
 	//}
 
 	// 피라미드를 쌓아라 - 프로젝트
-	int floor;
-	printf("몇 층으로 쌓겠느냐?");
-	scanf_s("%d", &floor);
-	for (int i = 0; i < floor; i++) {
-		for (int j = i; j < floor - 1; j++) {
-			printf(" ");
-		}
-		for (int k = 0; k < i * 2 + 1; k = k++) {
-			printf("*");
-		}
-		printf("\n");
-	}
+	//int floor;
+	//printf("몇 층으로 쌓겠느냐?");
+	//scanf_s("%d", &floor);
+	//for (int i = 0; i < floor; i++) {
+	//	for (int j = i; j < floor - 1; j++) {
+	//		printf(" ");
+	//	}
+	//	for (int k = 0; k < i * 2 + 1; k = k++) {
+	//		printf("*");
+	//	}
+	//	printf("\n");
+	//}
 
 
 	return 0;
